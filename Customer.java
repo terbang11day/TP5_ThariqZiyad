ca49bacdffbc7e1da83c3dc78c1b5727efb4e14b
terbang11day <thariqziyad48@gmail.com>
2023-08-05 22:57:24 +0700

    melengkapi class customer dan fruit

--- Customer.java
+++ Customer.java
@@ -1,3 +1,11 @@
 public class Customer {
-    // TODO: Implement this class.
+    private String name;
+    private String address;
+
+    public Customer(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // TODO: Implement any additional methods and fields as needed.
 }
--- Fruit.java
+++ Fruit.java
@@ -1,3 +1,11 @@
-public class Fruit {
-    // TODO: Implement this class.
+public class Fruit extends Product {
+    private String name;
+    private int price;
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // TODO: Implement any additional methods and fields as needed.
 }
